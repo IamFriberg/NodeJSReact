08000303d7391f4a2c47346689465820eabf123d
Karl FRIBERG <karl.friberg@soprasteria.com>
2019-10-07 14:03:29 +0200

    Added rest API endpoints and client connection.

--- api/app.js
+++ api/app.js
@@ -3,15 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//Add CORS to our API to allow cross-origin requests.
+var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//Require login
+var testAPIRouter = require('./routes/testAPI');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//Use CORS to our API to allow cross-origin requests.
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//require login
+app.use('/testAPI', testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
