89e3253ef8c0796e9c0b6ff0dafcbc5d61e4f6c1
IamFriberg <karlfriberg@hotmail.com>
2019-10-11 01:51:10 +0200

    Added basic login and logout functionality to Node and React application.

--- api/app.js
+++ api/app.js
@@ -1,12 +1,12 @@
 // Requiring necessary npm packages
 var express = require("express");
+var cors = require("cors");
 var bodyParser = require("body-parser");
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 //
-// Setting up port and requiring models for syncing
-//var PORT = process.env.PORT || 8080;
+// Requiring models for syncing
 var db = require("./models");
 //
 // Creating express app and configuring middleware needed for authentication
@@ -25,123 +25,23 @@ app.use(passport.session());
 console.log("Requiring our routes");
 //
 // Requiring our routes
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
 var htmlRouter = require('./routes/html-routes');
 var usersRouter = require('./routes/api-routes');
+var userRouter = require('./routes/user');
+var testAPIRouter = require('./routes/testAPI');
+
+//Add CORS to our API to allow cross-origin requests.
+app.use(cors());
 
 app.use('/', htmlRouter);
 app.use('/', usersRouter);
+app.use('/user', userRouter);
+app.use('/testAPI', testAPIRouter);
 
 //
 // Syncing our database and logging a message to the user upon success
 console.log("Syncing our database");
 db.sequelize.sync();
 
-// db.sequelize.sync().then(function() {
-//   app.listen(PORT, function() {
-//     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-//   });
-// });
-
 module.exports = app;
 
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// //Add CORS to our API to allow cross-origin requests.
-// var cors = require("cors");
-
-// //Use passport for session and login
-// var session = require("express-session");
-// var bodyParser = require("body-parser");
-// var passport = require("./config/passport");
-// // var LocalStrategy = require('passport-local').Strategy;
-
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-// //Test
-// var testAPIRouter = require('./routes/testAPI');
-// //Require login
-// var loginRouter = require('./routes/login');
-// //Import the models folder
-// var db = require("./models");
-
-// // Creating express app and configuring middleware needed for authentication
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// //Use passport for session and login
-// // passport.use(new LocalStrategy(
-// //   function(username, password, done) {
-// //     User.findOne({ username: username }, function(err, user) {
-// //       if (err) { return done(err); }
-// //       if (!user) {
-// //         return done(null, false, { message: 'Incorrect username.' });
-// //       }
-// //       if (!user.validPassword(password)) {
-// //         return done(null, false, { message: 'Incorrect password.' });
-// //       }
-// //       return done(null, user);
-// //     });
-// //   }
-// // ));
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// //Use CORS to our API to allow cross-origin requests.
-// app.use(cors());
-// //Use passport for session and login
-// app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// // We need to use sessions to keep track of our user's login status
-// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// //Test
-// app.use('/testAPI', testAPIRouter);
-// //require login
-// app.use('/login', loginRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// //
-// //this will listen to and show all activities on our terminal to 
-// //let us know what is happening in our app
-// // Syncing our database and logging a message to the user upon success
-// db.sequelize.sync().then(function() {
-//   app.listen(PORT, function() {
-//     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-//   });
-// });
-
-// module.exports = app;
